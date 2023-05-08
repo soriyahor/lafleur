c573754306cc9f7f98027d0715160c23c5e54ec2
Soriya Hor <hor.soriya@gmail.com>
2023-05-08 21:10:25 +0200

    js commenté

--- public/main.js
+++ public/main.js
@@ -36,40 +36,24 @@ var completed = 0,
     1157, //guava
     1230, //banana
     1298, //cherry
-
-    // 0, //orange
-    //     138, //number 7
-    //     277, //bar
-    //     415, //guava
-    //     554, //banana
-    //     692, //cherry
-    //     830, //orange
-    //     969, //number 7
-    //     1108, //bar
-    //     1246, //guava
-    //     1385, //banana
-    //     1523, //cherry
-    //     1661, //orange
-    //     1800, //number 7
-    //     1939, //bar
-    //     2077, //guava
-    //     2216, //banana
-    //     2354 //cherry
   ];
 
 var win = [];
-win[0] = win[830] = win[1661] = 1;
-win[138] = win[969] = win[1800] = 2;
-win[277] = win[1108] = win[1939] = 3;
-win[415] = win[1246] = win[2077] = 4;
-win[554] = win[1385] = win[2216] = 5;
-win[692] = win[1523] = win[2354] = 6;
+win[0] = win[454] = win[913] = 1;
+win[80] = win[539] = win[1000] = 2;
+win[165] = win[624] = win[1085] = 3;
+win[237] = win[696] = win[1157] = 4;
+win[310] = win[769] = win[1230] = 5;
+win[378] = win[837] = win[1298] = 6;
 
 var start = document.getElementById("start");
 start.addEventListener("click", spin);
 
 var result = document.getElementById("resultat");
 
+/**
+ * Permet de faire tourner les trois roulettes
+ */
 function spin() {
 
   // permets d'attribuer un index de position aléatoire pour les trois roulettes, qui seront utilisées pour initialiser la position de départ de chaque roulette lors du lancement de la fonction spin()
@@ -99,6 +83,7 @@ function spin() {
     }
   }, speed);
 
+  //  anime la deuxieme roulette en modifiant la position de l'image de fond à chaque itération, jusqu'à ce que la roulette tourne un certain nombre de fois (count > 50)
   var blur2 = setInterval(function () {
     roulette2.style.backgroundPosition = "0px " + posArr[roulette2Pos] + "px";
     roulette2Pos++;
@@ -112,6 +97,7 @@ function spin() {
     }
   }, speed);
 
+  //  anime la troisieme roulette en modifiant la position de l'image de fond à chaque itération, jusqu'à ce que la roulette tourne un certain nombre de fois (count > 50)
   var blur3 = setInterval(function () {
     roulette3.style.backgroundPosition = "0 " + posArr[roulette3Pos] + "px";
     roulette3Pos++;
@@ -125,8 +111,11 @@ function spin() {
   }, speed);
 }
 
+// Permet attribuer le lot gagné
 function checkWin() {
   completed++;
+
+  // envoie une requête XMLHttpRequest pour récupérer les données des loteries à partir d'un fichier JSON.
   if (completed == 3) {
     let loteries;
     const xhttp = new XMLHttpRequest();
@@ -147,6 +136,8 @@ function checkWin() {
     xhttp.setRequestHeader("Content-Type", "application/json");
     xhttp.send();
 
+
+    // calcule la somme de toutes les quantités restantes de chaque loterie et stocke le résultat dans la variable sum.
     let sum = 0;
 
     loteries.forEach((element) => {
@@ -164,16 +155,19 @@ function checkWin() {
     });
 
     // console.log("loteries", loteries);
-
+// crée un tableau array qui contient tous les taux de gain des loteries arrondis à deux décimales.
     let array = ["0"];
     loteries.forEach((element) => {
       array.push(Number(element.winRate.toFixed(2)));
     });
     // console.log("arr", array);
 
+    // génère un nombre aléatoire rate arrondi à deux décimales
     let rate = Math.random().toFixed(2);
     // console.log("rate", rate);
 
+
+    // parcourt le tableau array et détermine le lot gagné en fonction du taux de gain et du nombre aléatoire généré.
     let lot = -1;
     let max = 1,
       min = 0;
@@ -193,18 +187,20 @@ function checkWin() {
 
     // console.log("min max index", min, max, lot);
 
+// affiche le résultat sur les trois roulettes et affiche le produit gagnant sur la page web
     roulette1.style.backgroundPosition = "0px " + posArr[lot] + "px";
     roulette2.style.backgroundPosition = "0px " + posArr[lot] + "px";
     roulette3.style.backgroundPosition = "0 " + posArr[lot] + "px";
 
     var res =
-      "JACKPOT !! Vous avez gagné un lot : " + loteries[lot - 1].produit;
+      "Bravo !! Vous avez gagné un lot : " + loteries[lot - 1].produit;
     result.innerHTML = res;
     start.style.pointerEvents = "auto";
     completed = 0;
 
     document.getElementById("start").style.display = 'none';
 
+    // réinitialise les variables et les éléments de la page web pour permettre une nouvelle partie
     setTimeout(() => {
       document
         .getElementsByName("idLoterie")[0]
