1d41e3da515d6cd5df5d8389efc536f99c29fa31
Soriya Hor <hor.soriya@gmail.com>
2023-05-15 00:25:52 +0200

    rajout de style

--- App/vue/v_blog.php
+++ App/vue/v_blog.php
@@ -2,6 +2,7 @@
     <h1 class="titreBlog">Blog</h1>
 
     <div class="blog">
+    <img src="./public/images/cms.png" alt="blog de guillaume" class="cms">
         <h2> Découvrez le blog passionnant de Guillaume Cholet !</h2><br>
         <p>Avec sa passion pour la photographie, Guillaume capture la magie des paysages naturels et partage son amour pour sa région à travers ses articles. Venez découvrir ses magnifiques clichés et ses articles. N'hésitez pas à nous rejoindre sur notre blog en cliquant sur le lien ci-dessous pour une expérience immersive dans la nature et la beauté de la campagne. </p>
         <a href="https://hor.needemand.com/lafleur/wordpress/">Le blog de Guillaume</a><br><br>
